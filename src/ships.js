3d7c3bebb1b7405870915ee16c727de22ce38e98
Chenhao Wang <implexrr@gmail.com>
2025-02-09 20:48:40 -0500

    create rough outline of ships.js fxns

---
+++ src/ships.js
@@ -0,0 +1,51 @@
+const shipTypes = {
+  carrier: 5, battleship: 4, cruiser: 3, submarine: 2, destroyer: 1,
+};
+
+function createSectionsHitArr(x, y, orientation, length) {
+  const sectionsHitArr = {};
+  for (let i = 0; i < length; i += 1) {
+    if (orientation === 'horizontal') {
+      sectionsHitArr[`${x + i}, ${y}`] = false;
+    } else if (orientation === 'vertical') {
+      sectionsHitArr[`${x}, ${y + i}`] = false;
+    } else {
+      throw new Error('Not a valid orientation');
+    }
+  }
+  return sectionsHitArr;
+}
+
+export default function createShip(x, y, orientation, type) {
+  const length = shipTypes[type];
+  const sectionsHit = createSectionsHitArr(x, y, orientation, length);
+  let hits = 0;
+
+  function getShipLength() {
+    return length;
+  }
+
+  function isSectionHit(a, b) {
+    return sectionsHit[`${a}, ${b}`];
+  }
+
+  function isShipSunk() {
+    return length === hits;
+  }
+
+  function hitShip(a, b) {
+    if (sectionsHit[`${a}, ${b}`] === false) {
+      sectionsHit[`${a}, ${b}`] = true;
+      hits += 1;
+      console.log('hit!');
+    }
+  }
+
+  function getHits() {
+    return hits;
+  }
+
+  return {
+    getShipLength, isSectionHit, isShipSunk, hitShip, getHits,
+  };
+}
---
+++ src/tests/ship.test.js
@@ -0,0 +1,16 @@
+import createShip from "../ships";
+
+// TODO: Skeleton, change later
+// describe('sums correctly', () => {
+//   test('adds 2 positives', () => {
+//     expect(sum(1,2)).toBe(3);
+//   });
+
+//   test('adds 2 negatives', () => {
+//     expect(sum(-5, -10)).toBe(-15);
+//   });
+
+//   test('adds a positive and a negative', () => {
+//     expect(sum(1, -2)).toBe(-1);
+//   });
+// });
