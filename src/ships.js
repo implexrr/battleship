9d8f63e767bd69c9971376963ea417b325a4a7a4
Chenhao Wang <implexrr@gmail.com>
2025-02-14 12:13:45 -0500

    add comments to ship fxn

--- src/ships.js
+++ src/ships.js
@@ -2,6 +2,7 @@ const shipLengths = {
   carrier: 5, battleship: 4, cruiser: 3, submarine: 2, destroyer: 1,
 };
 
+// Create array of ship sections for a given ship/orientation
 function createSectionsHitArr(x, y, orientation, length) {
   const sectionsHitArr = {};
   for (let i = 0; i < length; i += 1) {
@@ -16,6 +17,7 @@ function createSectionsHitArr(x, y, orientation, length) {
   return sectionsHitArr;
 }
 
+// Create a ship object
 function makeShip(x, y, orientation, type) {
   const length = shipLengths[type];
   const sectionsHit = createSectionsHitArr(x, y, orientation, length);
@@ -33,6 +35,7 @@ function makeShip(x, y, orientation, type) {
     return length === hits;
   }
 
+  // Changes a setion in the ship from <shipname> to 'sunk', then increases # of hits on ship
   function hitShip(a, b) {
     if (sectionsHit[`${a}, ${b}`] === false) {
       sectionsHit[`${a}, ${b}`] = true;
@@ -44,12 +47,16 @@ function makeShip(x, y, orientation, type) {
     return hits;
   }
 
+  function getShipHealth() {
+    return length - hits;
+  }
+
   function getShipCoord() {
     return sectionsHit;
   }
 
   return {
-    getShipLength, isSectionHit, isShipSunk, hitShip, getHits, getShipCoord,
+    getShipLength, isSectionHit, isShipSunk, hitShip, getHits, getShipCoord, getShipHealth,
   };
 }
 
