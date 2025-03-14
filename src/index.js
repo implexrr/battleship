20b2860b2285978d552830558c3b2bfd2784bfbe
Chenhao Wang <implexrr@gmail.com>
2025-03-14 15:28:40 -0400

    add tests for 2 mock games

--- src/index.js
+++ src/index.js
@@ -2,17 +2,3 @@ import './main.css';
 import { makeShip } from './ships';
 import initializeGameboard from './gameBoard';
 import { shipLengths } from './ships';
-
-// Object.keys(shipLengths).forEach((shipType) => console.log(shipType));
-
-// const gameboardP1 = initializeGameboard('player1');
-// gameboardP1.placeShip(9, 7, 'vertical', 'destroyer');
-// console.log(gameboardP1.isShipFullyHere(9, 8, 'vertical', 'destroyer'));
-// console.log(gameboardP1.getBoard());
-
-// gameboardP1.registerHit(0, 0);
-// gameboardP1.registerHit(1, 0);
-// gameboardP1.registerHit(5,5);
-// gameboardP1.registerHit(5,6);
-// gameboardP1.registerHit(5,7);
-// gameboardP1.registerHit(5,8);
