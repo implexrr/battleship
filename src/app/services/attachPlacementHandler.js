8886b7131560772716d7563b7c828ae729f729b4
Chenhao Wang <implexrr@gmail.com>
2025-03-27 19:52:38 -0400

    update tdl

--- src/app/services/attachPlacementHandler.js
+++ src/app/services/attachPlacementHandler.js
@@ -10,12 +10,12 @@ export default function attachPlacementHandler(cell, gameboard, cellMap) {
     const row = Number(cell.dataset.row);
     const col = Number(cell.dataset.col);
 
-    // TDL: Combine 2 TDLs below, gameboard.placeShip and colorShip into one fxn?
     try {
       // Attempts to place the ship on the gameboard at the clicked position
       gameboard.placeShip(row, col, orientation, shipType);
       // Visually updates the UI to reflect the placed ship
       colorShip(cellMap, row, col, orientation, shipType);
+      // Checks to see if placement is complete, then transitions to the shooting page if it is
       checkPlacementCompleteness(gameboard);
     } catch (err) {
       // Logs error and prints current gameboard state for debugging
