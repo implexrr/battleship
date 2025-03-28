74687a95c5980195ec86cf9c7f82282de10dedab
Chenhao Wang <implexrr@gmail.com>
2025-03-28 13:08:19 -0400

    minor css changes, give ids to buttons

--- src/app/components/buttons.js
+++ src/app/components/buttons.js
@@ -1,8 +1,9 @@
 import gameStateManager from '../controllers/gameStateManager';
+import synthesizeElement from '../utils/synthesizeElement';
 
 // Create a "Play Now" button that sets the game state to 'placement'
 const playNowButtonEl = () => {
-  const el = document.createElement('button');
+  const el = synthesizeElement('button', { id: 'placement-button' });
   el.textContent = 'Play Now';
   el.addEventListener('click', () => { gameStateManager.setState('placement'); });
   return el;
@@ -10,7 +11,7 @@ const playNowButtonEl = () => {
 
 // Create a "Go to initial" button that sets the game state to 'initial'
 const initialButtonEl = () => {
-  const el = document.createElement('button');
+  const el = synthesizeElement('button', { id: 'initial-button' });
   el.textContent = 'Start Over';
   el.addEventListener('click', () => { gameStateManager.setState('initial'); });
   return el;
@@ -18,7 +19,7 @@ const initialButtonEl = () => {
 
 // Creates a "Go to gameOver" button that sets the game state to 'gameOver'
 const gameOverButtonEl = () => {
-  const el = document.createElement('button');
+  const el = synthesizeElement('button', { id: 'game-over-button' });
   el.textContent = 'Go to gameOver';
   el.addEventListener('click', () => { gameStateManager.setState('gameOver'); });
   return el;
