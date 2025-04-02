d68b01bea61385e656c3a5249ab4c70c6bab3f9b
Chenhao Wang <implexrr@gmail.com>
2025-04-02 14:13:28 -0400

    add persistent footer

--- src/app/components/footer/footer.js
+++ src/app/components/footer/footer.js
@@ -1,8 +1,25 @@
 import synthesizeElement from '../../utils/synthesizeElement';
 
+const copyright = () => {
+  const el = synthesizeElement('div', { id: 'description' });
+  el.textContent = 'Copyright © 2025 implexrr';
+  return el;
+};
+
+const symbol = () => {
+  const el = synthesizeElement('div', { id: 'github-link' });
+  return el;
+};
+
+const link = () => {
+  const el = synthesizeElement('a', { href: 'https://github.com/implexrr/battleship' });
+  el.append(symbol());
+  return el;
+};
+
 const footerEl = () => {
   const el = synthesizeElement('div', { id: 'footer' });
-  el.textContent = 'TDL: insert footer later';
+  el.append(copyright(), link());
   return el;
 };
 
