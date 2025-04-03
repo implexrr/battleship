1b1a87552dbf856ac978cc41cdb30e49d759dc00
Chenhao Wang <implexrr@gmail.com>
2025-04-03 18:02:54 -0400

    move repopulation code to diff file

--- src/app/components/pages/shootingPage/boards.js
+++ src/app/components/pages/shootingPage/boards.js
@@ -3,7 +3,6 @@ import { boardContainerEl, boardsContainerEl } from '../../../containers';
 import * as services from '../../../services';
 import { initializeGameboard, GAMEBOARD_LENGTH } from '../../../gameMechanics/gameBoard';
 import cellEl from '../../cell';
-import gameStateManager from '../../../controllers/gameStateManager.js';
 import boardLabelEl from '../../labels/boardLabel';
 
 const playerCellMap = {};
@@ -19,21 +18,15 @@ const aiBoardEl = () => {
   const el = synthesizeElement('div', { class: 'board ai' });
 
   // Repopulates the AI board
-  const fleetCoord = gameStateManager.getPlayerFleets().ai;
-  Object.keys(fleetCoord).forEach((ship) => {
-    gameboard.placeShip(
-      fleetCoord[ship].row,
-      fleetCoord[ship].col,
-      fleetCoord[ship].orientation,
-      ship,
-    );
-  });
+  services.repopulate(gameboard, 'ai');
 
   // Generate and append each cell based on the board matrix
   for (let i = 0; i < GAMEBOARD_LENGTH; i += 1) {
     for (let j = 0; j < GAMEBOARD_LENGTH; j += 1) {
       const cell = cellEl(i, j, 'ai', 'water', 'shooting');
       services.updateCellMap(cellMap, i, j, cell);
+      // TDL: add event listener for shooting (click), which will update both boards
+      // TDL p2 Create ai firing logic
       el.append(cell);
     }
   }
@@ -48,15 +41,7 @@ const playerBoardEl = () => {
   const el = synthesizeElement('div', { class: 'board player' });
 
   // Repopulates the player board
-  const fleetCoord = gameStateManager.getPlayerFleets().player;
-  Object.keys(fleetCoord).forEach((ship) => {
-    gameboard.placeShip(
-      fleetCoord[ship].row,
-      fleetCoord[ship].col,
-      fleetCoord[ship].orientation,
-      ship,
-    );
-  });
+  services.repopulate(gameboard, 'player');
 
   // Generate and append each cell based on the board matrix
   for (let i = 0; i < GAMEBOARD_LENGTH; i += 1) {
--- src/app/services/index.js
+++ src/app/services/index.js
@@ -2,3 +2,4 @@ export { colorBoard, colorShip } from './color';
 export { default as populateGameboardMatrix } from './populate';
 export { updateCellMap, extractCellMap } from './cellMap';
 export { default as showWarning } from './showWarning';
+export { default as repopulate } from './repopulate';
